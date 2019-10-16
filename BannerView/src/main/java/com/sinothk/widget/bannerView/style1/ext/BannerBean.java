6d7333b5482621086ce52d727c614fbb17ca72b9
LiangYT <381518188@qq.com>
2019-10-16 17:12:01 +0800

    升级环境，扩展加载drawable，file文件！

--- BannerView/src/main/java/com/sinothk/widget/bannerView/style1/ext/BannerBean.java
+++ BannerView/src/main/java/com/sinothk/widget/bannerView/style1/ext/BannerBean.java
@@ -14,22 +14,7 @@ public class BannerBean {
 
     private String title;
     private Object imgUrl;
-
-//    private String openUrl;
-
-    // url,R.drawable.*
-//    private Intent intent;
-//    private String openUrl;
-
     private String bizId;
-
-//    public static BannerBean getLocEntity(String title, Object imgUrl, String bizId) {
-//        BannerBean bean = new BannerBean();
-//        bean.setTitle(title);
-//        bean.setImgUrl(imgUrl);
-//        bean.setBizId(bizId);
-//        return bean;
-//    }
 
     public static BannerBean getEntity(String title, Object imgUrl, String bizId) {
         BannerBean bean = new BannerBean();
@@ -62,83 +47,4 @@ public class BannerBean {
     public void setBizId(String bizId) {
         this.bizId = bizId;
     }
-
-    //    public BannerBean(String title, String openUrl) {
-//        this.img = img;
-//        this.openUrl = openUrl;
-//    }
-
-//    @Deprecated
-//    public BannerBean(Object img) {
-//        this.img = img;
-//    }
-//
-//    public BannerBean(String img, String openUrl) {
-//        this.img = img;
-//        this.openUrl = openUrl;
-//    }
-//
-//    public BannerBean(String title, String img, String openUrl) {
-//        this.title = title;
-//        this.img = img;
-//        this.openUrl = openUrl;
-//    }
-//
-//    @Deprecated
-//    public BannerBean(String img, Intent intent) {
-//        this.img = img;
-//        this.intent = intent;
-//    }
-//
-//    @Deprecated
-//    public BannerBean(String title, String img, Intent intent) {
-//        this.title = title;
-//        this.img = img;
-//        this.intent = intent;
-//    }
-
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public Object getImg() {
-//        return img;
-//    }
-//
-//    public void setImg(Object img) {
-//        this.img = img;
-//    }
-//
-//    public Intent getIntent() {
-//        return intent;
-//    }
-//
-//    public void setIntent(Intent intent) {
-//        this.intent = intent;
-//    }
-//
-//    public String getOpenUrl() {
-//        return openUrl;
-//    }
-//
-//    public void setOpenUrl(String openUrl) {
-//        this.openUrl = openUrl;
-//    }
-//
-//    public String getBizId() {
-//        return bizId;
-//    }
-//
-//    public void setBizId(String bizId) {
-//        this.bizId = bizId;
-//    }
-//
-//    public BannerBean setBizId(BannerBean bannerBean, String bizId) {
-//        bannerBean.setBizId(bizId);
-//        return bannerBean;
-//    }
 }
